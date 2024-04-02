b5eac959f7dc8559cb8d2fea791daf96b9a66725
botslab <jordan.santos@botslab.com>
2024-04-02 20:21:10 -0300

    commit test

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 import pywinauto
 from pywinauto.application import Application
 from pywinauto.keyboard import send_keys
+from selenium import webdriver
 
 app = Application().start(cmd_line=r'"C:\Users\maema\Downloads\ACME-System3-v0.1\ACME-System3-v0.1\ACME-System3.exe"')
 time.sleep(2)
