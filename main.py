71d9466c8e8368dd85542bd71c2b9e51f2fb67a1
botslab <jordan.santos@botslab.com>
2024-04-02 20:17:54 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import time
+import pywinauto
+from pywinauto.application import Application
+from pywinauto.keyboard import send_keys
+
+app = Application().start(cmd_line=r'"C:\Users\maema\Downloads\ACME-System3-v0.1\ACME-System3-v0.1\ACME-System3.exe"')
+time.sleep(2)
+app.ACMESystem3.Exit.click()
+
